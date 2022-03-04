6eaed5f8835abcc9667dea5bdbdbf35bbd0fa767
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-03-04 15:40:43 +0800

    Initial Commit

---
+++ ChallengeTaskGroup/ChallengeTaskGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeTaskGroupApp.swift
+//  ChallengeTaskGroup
+//
+//  Created by Giovanni Gaffé on 2022/3/4.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeTaskGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
