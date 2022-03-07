33b577c4ab06a0b5f7a92e527ce9d9f4f17af5be
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-03-07 10:38:05 +0800

    set json model

--- ChallengeTaskGroup/decodingJson.swift
+++ ChallengeTaskGroup/decodingJson.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct Message: Decodable {
+    let id: Int
+    let from: String
+    let message: String
+}
+
+struct User: Decodable {
+    let username: String
+    let favorites: Set<Int>
+    let messages: [Message]
+}
+
+enum FetchResult {
+    case username(String)
+    case favorites(Set<Int>)
+    case messages([Message])
+    
+}
